974a677114b171f61a2fdfdfe3539077eb19dbb0
Prajakta <prajakta.n.mane@gmail.com>
2014-12-11 17:22:07 -0700

    Added comment

--- refVsPointers.cpp
+++ refVsPointers.cpp
@@ -8,6 +8,7 @@ int main(void)
     int  x =0;
     int &r = x;
     int *p = &x;
+    // address of the reference returns the address of the variable it is pointing to 
     int *p2 = &r;
     assert(p==p2);
    
